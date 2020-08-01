d89942f3e6f62230feac47dd9f919407dceafa1d
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-08-01 13:31:56 -0700

    Update to js
    Moved some stuff around and commented out the mosaic stuff for testing. Also added the campaign header alignment function

--- js/index.js
+++ js/index.js
@@ -1,40 +1,45 @@
 $(document).ready(function () {
 
     NavBar();
+    ImgClick();
+    CampaignHeaderAlignment();
+    // MosaicRow();
+
     $(window).resize(function () {
         NavBar();
+        CampaignHeaderAlignment();
+        // MosaicRow();
     });
 
-    $('img').click(function () {
-        source = $(this).attr('src');
-        PopUp(source)
-    });
+
+
+
 
     // $('#about img').click(function () {
     //     source = $(this).attr('src');
     //     PopUp(source)
     // });
 
-    $('.candidates').slick({
-        infinite: true,
-        mobileFirst: true,
-        responsive: [
-            {
-                breakpoint: 0,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-        ]
-    });
+    // $('.candidates').slick({
+    //     infinite: true,
+    //     mobileFirst: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 0,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 1,
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1,
+    //             }
+    //         },
+    //     ]
+    // });
 
     // $('.mosaic-grids').slick({
     //     centerMode: false,
@@ -43,9 +48,16 @@ $(document).ready(function () {
     //     slidesToScroll: 1
     // });
 
-    $('.slick-prev').empty().append('<ion-icon name="caret-back-sharp"></ion-icon>');
-    $('.slick-next').empty().append('<ion-icon name="caret-forward-sharp"></ion-icon>');
+    // $('.slick-prev').empty().append('<ion-icon name="caret-back-sharp"></ion-icon>');
+    // $('.slick-next').empty().append('<ion-icon name="caret-forward-sharp"></ion-icon>');
 });
+
+function ImgClick() {
+    $('img').click(function () {
+        source = $(this).attr('src');
+        PopUp(source)
+    });
+}
 
 function PopUp(image) {
     $('.modal-body img').remove();
@@ -56,4 +68,22 @@ function NavBar() {
     var navHeight = $('nav').outerHeight(true);
     $('#home').css('margin-top', navHeight);
 }
+
+function CampaignHeaderAlignment() {
+    var windowWidth = $(window).outerWidth(true);
+    if (windowWidth > 767) {
+        var wrapWidth = $('.campaign-management .wrap').width();
+        var colWidth = $('.campaign-management .col-lg-5').outerWidth();
+        var campaignMargin = ((wrapWidth - (colWidth * 2)) / 2) + 15;
+        $('.campaign-management h4').css('margin-left', campaignMargin);
+    }
+}
+
+// function MosaicRow() {
+//     var containerWidth = $('#mosaic .container').width();
+//     var rowWidth = containerWidth / 3;
+//     var newRowWidth = 'repeat(3, ' + rowWidth + ')';
+//     console.log(rowWidth);
+//     $('.mosaic-grid').css('gridTemplateRows', rowWidth);
+// }
 
