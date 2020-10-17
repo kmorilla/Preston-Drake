2604cab53295cab3f38dd4881a0386126b8fba10
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2020-10-17 15:39:04 -0700

    Added the blog section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     ImgClick();
+    BlogSetup();
+    LoadMore();
+    LoadLess();
 });
 
 function ImgClick() {
@@ -14,3 +17,48 @@ function PopUp(image) {
     $('.modal-body img').remove();
     $('.modal-body').append('<img src="' + image + '" >');
 }
+
+function BlogSetup() {
+    let blog = '.blog-post';
+
+    $(blog + ':nth-child(n + 4)').hide();
+    $('.load-more-button').addClass('load-more-only-button');
+    $('.load-less-button').hide();
+}
+
+function LoadMore() {
+    let blog = '.blog-post';
+
+    $('.load-more-button').click(function () {
+        let visibleCount = $(blog + ':visible').length;
+
+        if (visibleCount < 4) {
+            $('.load-more-button').removeClass('load-more-only-button');
+            $('.load-less-button').show('duration: 600');
+        }
+
+        $(blog + ':nth-child(n + 4):nth-child(-n + ' + (visibleCount + 3) + ')').show('duration: 600');
+        $(blog + ':nth-child(n + ' + (visibleCount + 4) + ')').hide('duration: 600');
+    });
+}
+
+function LoadLess() {
+    let blog = '.blog-post';
+
+    $('.load-less-button').click(function () {
+        let visibleCount = $(blog + ':visible').length;
+        let hiddenCount = (visibleCount % 3);
+
+        if (hiddenCount == 0) {
+            $(blog + ':nth-child(n + ' + (visibleCount - 2) + ')').hide('duration: 600');
+        } else {
+            $(blog + ':nth-child(n + ' + (visibleCount - (hiddenCount - 1)) + ')').hide('duration: 600');
+        }
+
+        if (visibleCount < 7) {
+            $('.load-more-button').addClass('load-more-only-button');
+            $('.load-less-button').hide('duration: 600');
+        }
+    });
+}
+
